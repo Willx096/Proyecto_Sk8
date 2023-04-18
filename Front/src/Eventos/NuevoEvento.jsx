b0f5391a17e3253342f7741c012f4833c168ae29
Willx096 <guillem.lorca96@gmail.com>
2023-04-18 21:24:02 +0200

    Segundo commit NewEvent

--- Front/src/Eventos/NuevoEvento.jsx
+++ Front/src/Eventos/NuevoEvento.jsx
@@ -27,9 +27,21 @@ function NuevoEvento(props) {
    <Form className="form">
     <Form.Group className="mb-3" controlId="formBasicEmail">
       <Form.Label >Titulo</Form.Label>
-      <Form.Control type="title" placeholder="Introduce el nombre" />
+      <Form.Control type="title" placeholder="Inserta un titulo" />
       <Form.Label>Participantes</Form.Label>
-      <Form.Control type="participantes" placeholder="Introduce el apellido" />
+      <Form.Select aria-label="Default select example">
+      <option>Seleccióna el número de participantes</option>
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="3">4</option>
+      <option value="3">5</option>
+      <option value="3">6</option>
+      <option value="3">7</option>
+      <option value="3">8</option>
+      <option value="3">9</option>
+      <option value="3">10</option>
+    </Form.Select>
       <Form.Label>Nivel</Form.Label>
       <Form.Control type="level" placeholder="Introduce el correo" />
       <Form.Label>Lugar</Form.Label>
