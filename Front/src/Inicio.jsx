64a6063679233bacb9037aba36ee462b1ca4ed75
Christian Gómez <cgomezz.dev@gmail.com>
2023-04-26 17:33:13 +0200

    video de inicio

--- Front/src/Inicio.jsx
+++ Front/src/Inicio.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import { Container, Image } from "react-bootstrap";
-import encabezado from "./img/inicio.jpg";
+import encabezado from "../public/VideoInicio.mp4";
 import "./inicio.css";
 import Registro from "./Usuarios/Registro";
 
 function Inicio(props) {
   return (
     <>
-      <img
-        src={encabezado}
-        className="img-fluid" 
-        alt="imagen-inicio"
-      />
-      <hr/>
+      <div>
+        <video src={encabezado} autoPlay loop muted className="img-fluid" alt="imagen-inicio" />
+        <h1>Hello!</h1>
+      </div>
+      <hr />
       <Registro />
     </>
   );
