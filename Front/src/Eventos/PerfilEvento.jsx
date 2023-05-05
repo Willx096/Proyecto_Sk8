0fc83bd410b551e3ca5396de3bad1ea6877ec27b
Christian Gómez <cgomezz.dev@gmail.com>
2023-05-05 19:09:16 +0200

    usuario logeado = id_usuario  evento muestra edit

--- Front/src/Eventos/PerfilEvento.jsx
+++ Front/src/Eventos/PerfilEvento.jsx
@@ -51,45 +51,50 @@ function PerfilEvento(props) {
 
   console.log("provando", datos);
   //Tabla de eventos
-  const filas = datos.map((el, index) => ( // Crea una fila para cada evento encontrado
-    <Card.Body key={index}>
-      <Card.Title>{el.titulo}</Card.Title>
-      <Card.Subtitle className="mb-2 text-muted">
-        {el.descripcion}
-      </Card.Subtitle>
-      <Card.Text>Fecha: {el.fecha}</Card.Text>
-      <Card.Text>Dirección: {el.direccion}</Card.Text>
-      <Card.Text>Nivel: {el.nivel}</Card.Text>
-      <Card.Text>
-        Participantes: {el.Participacions.length}/{el.participantes}:
-      </Card.Text>
-      <Card.Text>
-        {/* Muestra los nkckames separados por comoas */}
-        {/* Participantes1:{" "}
+  const filas = datos.map(
+    (
+      el,
+      index // Crea una fila para cada evento encontrado
+    ) => (
+      <Card.Body key={index}>
+        <Card.Title>{el.titulo}</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">
+          {el.descripcion}
+        </Card.Subtitle>
+        <Card.Text>Fecha: {el.fecha}</Card.Text>
+        <Card.Text>Dirección: {el.direccion}</Card.Text>
+        <Card.Text>Nivel: {el.nivel}</Card.Text>
+        <Card.Text>
+          Participantes: {el.Participacions.length}/{el.participantes}:
+        </Card.Text>
+        <Card.Text>
+          {/* Muestra los nkckames separados por comoas */}
+          {/* Participantes1:{" "}
         <span>
           {el.Participacions.map((e) => e.Usuario?.nickname).join(", ")}
         </span>
         <br /> */}
-        {/* Muestra los nkckames sin separar por comoas y coje el id del usuario y te manda a perfi/id pero solo muestra tu usuario */}
-        Participantes:{" "}
-        <span>
-          {el.Participacions.map((e, i) => (
-            <Button key={i} onClick={() => goToPerfil(e.Usuario.id)}>
-              {" "}
-              {e.Usuario.nickname}
-            </Button>
-          ))}
-        </span>
-        <br />
-      </Card.Text>
-      <Card.Text>
-        Valoraciones: {el.Participacions.map((e) => e.valoracion).join("\n")}
-      </Card.Text>
-      <Card.Text>
-        Puntuacion: {el.Participacions.map((e) => e.puntuacion).join("\n")}
-      </Card.Text>
-    </Card.Body>
-  ));
+          {/* Muestra los nkckames sin separar por comoas y coje el id del usuario y te manda a perfi/id pero solo muestra tu usuario */}
+          Participantes:{" "}
+          <span>
+            {el.Participacions.map((e, i) => (
+              <Button key={i} onClick={() => goToPerfil(e.Usuario.id)}>
+                {" "}
+                {e.Usuario.nickname}
+              </Button>
+            ))}
+          </span>
+          <br />
+        </Card.Text>
+        <Card.Text>
+          Valoraciones: {el.Participacions.map((e) => e.valoracion).join("\n")}
+        </Card.Text>
+        <Card.Text>
+          Puntuacion: {el.Participacions.map((e) => e.puntuacion).join("\n")}
+        </Card.Text>
+      </Card.Body>
+    )
+  );
 
   function Apuntarse() {
     var myHeaders = new Headers();
@@ -111,7 +116,7 @@ function PerfilEvento(props) {
     fetch("http://localhost:5000/api/participacion/apuntarse", requestOptions)
       .then((response) => response.text())
       .then((result) => console.log(result))
-      .then(() => setApuntado(!apuntado))  // actualiza el estado de apuntado/desapuntado
+      .then(() => setApuntado(!apuntado)) // actualiza el estado de apuntado/desapuntado
       .catch((error) => console.log("error", error));
   }
 
@@ -119,15 +124,17 @@ function PerfilEvento(props) {
     <div>
       <h3>Informacion del evento</h3>
       <Card border="dark">
-        {filas}
-        {datos[0].participantes > datos[0].Participacions.length ? (
+        {filas}        
+        {userid === datos[0].id_usuario ? ( //Si el usuario logeado coincide con el id_usuario del evento le mostrara para editar
+          <Button variant="warning">Editar</Button>
+        ) : datos[0].participantes > datos[0].Participacions.length ? (
           // Si hay plazas disponibles, muestra un botón para apuntarse o desapuntarse llamando a la función Apuntarse
           <Button onClick={Apuntarse} variant={apuntado ? "danger" : "primary"}>
             {apuntado ? "Desapuntarse" : "Apuntarse"}
           </Button>
         ) : (
           // Si el evento está completo, muestra un mensaje
-          <h3>El evento esta completo</h3>
+          <h3>evento esta completo</h3>
         )}
       </Card>
     </div>
