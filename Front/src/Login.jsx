8b7b9abaaa0b9236e5e0a92e059de80de36ce368
Christian Gómez <cgomezz.dev@gmail.com>
2023-05-16 18:27:38 +0200

    100% done validate login

--- Front/src/Login.jsx
+++ Front/src/Login.jsx
@@ -2,39 +2,64 @@ import React from "react";
 import { useState } from "react";
 import { Button, Form, Modal } from "react-bootstrap";
 
-function Login({showLogin, iniciaSesion, setShowLogin} ) {
-  
-    const [email, setEmail] = useState("")
-    const [pswd, setPswd] = useState("")
+function Login({ showLogin, iniciaSesion, setShowLogin }) {
+  const [email, setEmail] = useState("");
+  const [pswd, setPswd] = useState("");
+  const [validated, setValidated] = useState(false);
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    console.log("enviant");
+    iniciaSesion(email, pswd);
+  };
 
   return (
     <>
-      <Modal show={showLogin} onHide={()=>setShowLogin(false)}>
-      
-        <Modal.Header closeButton>
-          <Modal.Title>Inicia sesión para empezar a Skeatear!</Modal.Title>
-          {/* la idea aqui es poner algo que no sea tan cutre pero que no se inciiar sesion y ya */}
-        </Modal.Header>
-        <Modal.Body>
-        <Form>
-            <Form.Group  className="mb-3" controlId="formBasicEmail">
+      <Modal show={showLogin} onHide={() => setShowLogin(false)}>
+        <Form onSubmit={onSubmit} validated={validated}>
+          <Modal.Header closeButton>
+            <Modal.Title>Inicia sesión para empezar a Skeatear!</Modal.Title>
+            {/* la idea aqui es poner algo que no sea tan cutre pero que no se inciiar sesion y ya */}
+          </Modal.Header>
+          <Modal.Body>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Correo Electrónico</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" value={email} onInput={(e) => setEmail(e.target.value)}/>
-                                    </Form.Group>
+              <Form.Control
+                required
+                type="email"
+                placeholder="Enter email"
+                value={email}
+                onInput={(e) => setEmail(e.target.value)}
+              />
+              <Form.Control.Feedback type="invalid">
+                Introduce un email válido.
+              </Form.Control.Feedback>
+            </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicpassword">
               <Form.Label>Contraseña</Form.Label>
-              <Form.Control type="password" placeholder="" value={pswd} onInput={(e) => setPswd(e.target.value)} />
-                   </Form.Group>
-            </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          
-          <Button variant="primary" onClick={()=>iniciaSesion(email, pswd)}>
-            Iniciar Sesión
-          </Button>
-        </Modal.Footer>
-       
+              <Form.Control
+                required
+                type="password"
+                placeholder=""
+                value={pswd}
+                onInput={(e) => setPswd(e.target.value)}
+              />
+              <Form.Control.Feedback type="invalid">
+                Contraseña no válida.
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button
+              variant="primary"
+              type="submit"
+              onClick={() => setValidated(true)}
+            >
+              Iniciar Sesión
+            </Button>
+          </Modal.Footer>
+        </Form>
       </Modal>
     </>
   );
