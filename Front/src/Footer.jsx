264ca3e2734e5ce4368a6c35c1d917cea7f9bc36
Christian Gómez <cgomezz.dev@gmail.com>
2023-05-19 01:25:57 +0200

    footer temporal echo

--- Front/src/Footer.jsx
+++ Front/src/Footer.jsx
@@ -1,33 +1,34 @@
 import React from "react";
 import { Container, Col, Row } from "react-bootstrap";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faInstagram, faFacebook} from "@fortawesome/free-solid-svg-icons";
 
 function Footer() {
   return (
     <>
-      <footer className="bg-light text-center text-lg-start">
-        <div className="container p-2">
-          <div className="row">
-            <div className="col-lg-6 col-md-12 mb-4 mb-md-0">
-              <h5 className="text-uppercase">Contaco</h5>
-              <p>62739839</p>
-              <p>skateZone@gmail.com</p>
+      <footer className="bg-dark text-center text-white">
+        <div className="container p-5 pb-0">
+          <section className="">
+            <div className="row d-flex justify-content-center">
+              <div className="col-auto">
+                <p className="pt-1">
+                  <strong>
+                    Para cualquier incidencia contacte a este email:
+                  </strong>
+                </p>
+              </div>
+
+              <div className="col-auto">
+                <div className="form-outline form-white mb-4">
+                  <p className="pt-1">skateZone@gmail.com</p>
+                </div>
+              </div>
             </div>
-            <div className="col-lg-6 col-md-12 mb-4 mb-md-0">
-              <h5 className="text-uppercase">Redes</h5>
-              <a>{/* <FontAwesomeIcon icon={faInstagram} /> */}</a>
-              <a href="">{/* <FontAwesomeIcon icon={faFacebook} /> */}</a>
-              <a href="">{/* <FontAwesomeIcon icon={faLinkedIn} /> */}</a>
-              <a href="">{/* <FontAwesomeIcon icon={faGitHub} />{" "} */}</a>
-            </div>
-          </div>
+          </section>
         </div>
 
-        <div className="text-center p-3">
+        <div class="text-center p-2 bg-dark">
           © 2023 Copyright:
-          <a className="text-dark" href="#">
-            Sk8Zone
+          <a class="text-white" href="https://skatezone.com/">
+            SkateZone
           </a>
         </div>
       </footer>
