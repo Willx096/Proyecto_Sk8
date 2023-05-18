5e9cff78c62e87621a3d16891e3f3094497f1e13
Christian Gómez <cgomezz.dev@gmail.com>
2023-05-18 02:23:40 +0200

    en proceso error al poner los iconos fontawsome

--- Front/src/Footer.jsx
+++ Front/src/Footer.jsx
@@ -1,13 +1,38 @@
 import React from "react";
 import { Container, Col, Row } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faInstagram, faFacebook} from "@fortawesome/free-solid-svg-icons";
 
-function Footer () {
+function Footer() {
+  return (
+    <>
+      <footer className="bg-light text-center text-lg-start">
+        <div className="container p-2">
+          <div className="row">
+            <div className="col-lg-6 col-md-12 mb-4 mb-md-0">
+              <h5 className="text-uppercase">Contaco</h5>
+              <p>62739839</p>
+              <p>skateZone@gmail.com</p>
+            </div>
+            <div className="col-lg-6 col-md-12 mb-4 mb-md-0">
+              <h5 className="text-uppercase">Redes</h5>
+              <a>{/* <FontAwesomeIcon icon={faInstagram} /> */}</a>
+              <a href="">{/* <FontAwesomeIcon icon={faFacebook} /> */}</a>
+              <a href="">{/* <FontAwesomeIcon icon={faLinkedIn} /> */}</a>
+              <a href="">{/* <FontAwesomeIcon icon={faGitHub} />{" "} */}</a>
+            </div>
+          </div>
+        </div>
 
-    return(
-        <Container>
-            
-        </Container>
-    )
+        <div className="text-center p-3">
+          © 2023 Copyright:
+          <a className="text-dark" href="#">
+            Sk8Zone
+          </a>
+        </div>
+      </footer>
+    </>
+  );
 }
 
-export default Footer
+export default Footer;
